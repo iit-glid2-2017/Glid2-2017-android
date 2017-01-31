f3887b4e96a9634786857b0f1b2094ce06b5f14a
Slim BENHAMMOUDA <slim.benhammouda@gmail.com>
2017-01-31 17:38:16 +0100

    first commit

---
+++ app/src/main/java/com/iit/glid2017/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iit.glid2017.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_relative);
+    }
+}
